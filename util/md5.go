1f79de9a1039f9b83206d3ffb741caf64ee07ee8
kerry <kerry@kerrydeMacBook-Pro.local>
2025-06-24 15:17:14 +0800

    +

--- util/md5.go
+++ util/md5.go
@@ -3,6 +3,7 @@ package util
 import (
 	"crypto/md5"
 	"fmt"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func MD5(str string) string {
@@ -10,4 +11,13 @@ func MD5(str string) string {
 	has := md5.Sum(data)
 	md5str := fmt.Sprintf("%x", has) //将[]byte转成16进制
 	return md5str
+}
+
+func HashPassword(pw string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(pw), bcrypt.DefaultCost)
+	return string(bytes), err
+}
+
+func CheckPassword(hashed, plain string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(hashed), []byte(plain)) == nil
 }
